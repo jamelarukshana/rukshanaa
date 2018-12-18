30e3a045ceef3388b48b4612e5b7c3ed526d3bce
jamelarukshana <42768504+jamelarukshana@users.noreply.github.com>
2018-12-18 12:23:22 +0530

    Create Alp.java

---
+++ Alp.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+import java.io.*;
+import java.lang.*;
+class Alp
+{
+public static void main(String args[])
+{
+int alp;
+Scanner sc=new Scanner(System.in);
+alp=next().charAt(0);
+System.out.println(+alp);
+if((alp>='a'&& alp<='z')||(alp>='A' && alp<='Z'))
+  System.out.println("alphabet");
+else
+  System.out.println("No");
+}
+}
