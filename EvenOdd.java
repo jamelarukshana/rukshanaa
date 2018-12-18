aed4531f93ba3e597dec8f5bd899e4348c50d3ef
jamelarukshana <42768504+jamelarukshana@users.noreply.github.com>
2018-12-18 10:10:13 +0530

    Update EvenOdd.java

--- EvenOdd.java
+++ EvenOdd.java
@@ -1,13 +1,13 @@
 import java.util.Scanner;
 class EvenOdd
 {
-public static voidmain(String args[])
+public static void main(String args[])
 {
 int value;
 Scanner sc=new Scanner(System.in);
-value=nextInt();
-System.out.println("Output:"+value);
-if(value/2)
+value=sc.nextInt();
+System.out.println(+value);
+if(value%2==0)
   System.out.println("Even");
 else
   System.out.println("Odd");
