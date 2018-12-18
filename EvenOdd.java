6d4ab2933ac40f739b39beeb629b22700b17f7de
jamelarukshana <42768504+jamelarukshana@users.noreply.github.com>
2018-12-18 10:02:55 +0530

    Create EvenOdd.java

---
+++ EvenOdd.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class EvenOdd
+{
+public static voidmain(String args[])
+{
+int value;
+Scanner sc=new Scanner(System.in);
+value=nextInt();
+System.out.println("Output:"+value);
+if(value/2)
+  System.out.println("Even");
+else
+  System.out.println("Odd");
+}
+}
