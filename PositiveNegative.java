db751d26a812696d2a5f4286637f2a09e3aab607
jamelarukshana <42768504+jamelarukshana@users.noreply.github.com>
2018-12-18 10:52:41 +0530

    Update PositiveNegative.java

--- PositiveNegative.java
+++ PositiveNegative.java
@@ -6,7 +6,7 @@ public static void main(String args[])
 int number;
 Scanner sc=new Scanner(System.in);
 number=sc.nextInt();
-System.out.println("Output:"+number);
+System.out.println(+number);
 if(num==0)
   System.out.println("Zero");
 if(num<0)
