9021aa333378435bb7348b0b72d7a6d77ef181a7
jamelarukshana <42768504+jamelarukshana@users.noreply.github.com>
2018-12-18 12:24:17 +0530

    Update Alp.java

--- Alp.java
+++ Alp.java
@@ -10,7 +10,7 @@ Scanner sc=new Scanner(System.in);
 alp=next().charAt(0);
 System.out.println(+alp);
 if((alp>='a'&& alp<='z')||(alp>='A' && alp<='Z'))
-  System.out.println("alphabet");
+  System.out.println("Alphabet");
 else
   System.out.println("No");
 }
