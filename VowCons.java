1d510fa57b68779ea6850ae9e33acac24429a77e
jamelarukshana <42768504+jamelarukshana@users.noreply.github.com>
2018-12-18 11:52:40 +0530

    Update VowCons.java

--- VowCons.java
+++ VowCons.java
@@ -11,5 +11,7 @@ if(ltr='A'||ltr='E'||ltr='I'||ltr='O'||ltr='U')
   System.out.println("vowels");
 if(ltr='B'||ltr='C'||ltr=='D'||ltr='F'||ltr='G'||ltr='H'||ltr='J'||ltr='K'||ltr='L'||ltr='M'||ltr='N'||ltr='P'||ltr='Q'||ltr='R'||ltr='S'||ltr='T'||ltr='V'||ltr='W'||ltr='X'||ltr='Y'||ltr='Z')
   System.out.println("consonants");
+else
+  System.out.println("invalid");
 }
 }
