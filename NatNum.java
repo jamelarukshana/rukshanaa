52634a99f8e23caf58bb816fc70e3c2509a063ec
jamelarukshana <42768504+jamelarukshana@users.noreply.github.com>
2018-12-19 12:01:59 +0530

    Create NatNum.java

---
+++ NatNum.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.Scanner;
+class NatNum
+{
+public static void main(String args[])
+{
+int i,n,sum=0;
+Scanner sc=new Scanner(System.in);
+n=sc.nextInt();
+for(i=0;i<=n;i++)
+  sum=sum+i;
+System.out.println(=sum);
+}
+}
