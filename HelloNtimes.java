4527f9a0bfe2531aa611f6b0bd02dceb540b6a5c
jamelarukshana <42768504+jamelarukshana@users.noreply.github.com>
2018-12-19 10:51:15 +0530

    Create HelloNtimes.java

---
+++ HelloNtimes.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.Scanner;
+class HelloNtimes
+{
+public static void main(String args[])
+{
+int i,n;
+Scanner sc=new Scanner(system.in);
+n=sc.nextInt();
+for(i=1;i<n;i++)
+System.out.println("Hello\n");
+
+}
+}
