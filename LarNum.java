e2059f55e36b4866472ebcadda494c06bbbcf937
jamelarukshana <42768504+jamelarukshana@users.noreply.github.com>
2018-12-19 09:17:07 +0530

    Create LarNum.java

---
+++ LarNum.java
@@ -0,0 +1,21 @@
+import java.lang;
+import java.io.*;
+import java.util.*;
+class LarNum
+{
+public static void main(String args[])
+{
+int a,b,c;
+Scanner sc=new Scanner(System.in);
+a=sc.nextInt();
+b=sc.nextInt();
+c=sc.nextInt();
+if(a>=b&&a>=c)
+System.out.println(+a);
+if(b>=a&&b>=c)
+System.out.println(+b);
+if(c>=a&&c>=b)
+System.out.println(+c);
+
+}
+}
